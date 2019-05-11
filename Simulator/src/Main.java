6453a61f67d07350538f9058fd37106bb78fc9e7
DOSLAN <49920936+DOSLAN@users.noreply.github.com>
2019-05-12 00:38:57 +0600

    Add files via upload
    more details

--- Simulator/src/Main.java
+++ Simulator/src/Main.java
@@ -5,18 +5,12 @@ public class Main
     public static void main(String[] args)
     {
         Modeling modeling = new Modeling();
-        modeling.inputCamera(0 ,0 ,1000);
+        modeling.inputCamera(10 , 10 ,700);
+        modeling.inputLight(90 , 60);
 
 
 
-        modeling.addTriangle( 0 , 0 , 100 , 71 , 0 , 71 , 0  , 0 , 0 , new double[] {0, 0 ,0});
-        modeling.addTriangle( 100 , 0 , 0 , 71 , 0 , 71 , 0  , 0 , 0 , new double[] {0, 0 ,0});
-        modeling.addTriangle( 100 , 0 , 0 , 71 , 0 , -71 , 0  , 0 , 0 , new double[] {0, 0 ,0});
-        modeling.addTriangle( 0 , 0 , -100 , 71 , 0 , -71 , 0  , 0 , 0 , new double[] {0, 0 ,0});
-        modeling.addTriangle( 0 , 0 , 100 , -71 , 0 , 71 , 0  , 0 , 0 , new double[] {0, 0 ,0});
-        modeling.addTriangle( -100 , 0 , 0 , -71 , 0 , 71 , 0  , 0 , 0 , new double[] {0, 0 ,0});
-        modeling.addTriangle( -100 , 0 , 0 , -71 , 0 , -71 , 0  , 0 , 0 , new double[] {0, 0 ,0});
-        modeling.addTriangle( 0 , 0 , -100 , -71 , 0 , -71 , 0  , 0 , 0 , new double[] {0, 0 ,0});
+        modeling.addTriangle( 0 , 0 , 300 , 400 , 0 , -200 , -400  , 0 , -200 , new double[] {0, 200 ,255});
         modeling.toScreen();
 
 
@@ -25,7 +19,7 @@ public class Main
         JFrame frame = new JFrame("Drawing triangle");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.add(modeling);
-        frame.setSize(300, 300);
+        frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
         frame.setVisible(true);
     }
 
--- Simulator/src/Modeling.java
+++ Simulator/src/Modeling.java
@@ -5,12 +5,12 @@ import java.awt.Color;
 import javax.swing.JFrame;
 
 class Modeling extends JPanel {
-    private double camX, camY, camZ;
+    private double camX, camY, camZ , lightX , lightY , lightZ;
 
 
     void inputCamera(double latitude, double longtitude, double distance) {
 
-        latitude = Math.toRadians(latitude);
+        latitude = -1*Math.toRadians(latitude);
         longtitude = Math.toRadians(longtitude);
         camX = distance * Math.cos(longtitude) * Math.sin(latitude);
         camY = distance * Math.cos(longtitude) * Math.cos(latitude);
@@ -18,14 +18,27 @@ class Modeling extends JPanel {
     }
 
 
+
+
+    void inputLight(double latitude, double longtitude) {
+
+        latitude = -1*Math.toRadians(latitude);
+        longtitude = Math.toRadians(longtitude);
+        lightX = Math.cos(longtitude) * Math.sin(latitude);
+        lightY = Math.cos(longtitude) * Math.cos(latitude);
+        lightZ = Math.sin(longtitude);
+    }
+
+
     private double[] getCoordinates(double x, double y, double z) {
 
 
         double multiple = camX * camX + camY * camY + camZ * camZ;
-        double ratio = multiple / (multiple - camX * x - camY * y - camZ * z);
-        double rX = ratio * (x - camX) + camX;
-        double rY = ratio * (y - camY) + camY;
-        double rZ = ratio * (z - camZ) + camZ;
+        double ratio = (multiple - camX * x - camY * y - camZ * z)/multiple  ;
+        double distance = ratio*Math.sqrt(multiple);
+        double rX = (x - camX)/ratio + camX;
+        double rY = (y - camY)/ratio + camY;
+        double rZ = (z - camZ)/ratio + camZ;
         double c = (rZ * rZ * camZ * camZ) / (camX * camX + camY * camY);
         double sign = -1;
         if (camX != 0) {
@@ -36,17 +49,19 @@ class Modeling extends JPanel {
         double resultY = Math.signum(rZ) * Math.sqrt(rZ * rZ + c);
 
 //        System.out.printf("%n     %s     %s     %s     %s", x, rX, sign, resultX);
-        return (new double[]{resultX, resultY});
+        return (new double[]{resultX, resultY , distance});
     }
 
 
     private double[][][] triangles3d = new double[10000][1][];
-    private double[][][] triangles2d = new double[10000][2][];
-    private double[][][] triangleNull = new double[10000][2][];
+    private int[][][] triangles2d = new int[10000][2][];
+    private int[][][] triangleNull = new int[10000][2][];
     private int indexOfTriangles = 0;
 
 
     void addTriangle(double x1, double y1, double z1, double x2, double y2, double z2, double x3, double y3, double z3, double[] color) {
+
+
         double[][] thisList = new double[][]{{x1, y1, z1, x2, y2, z2, x3, y3, z3}, color};
         triangles3d[indexOfTriangles] = thisList;
         indexOfTriangles += 1;
@@ -68,7 +83,31 @@ class Modeling extends JPanel {
             double x3 = triangles3d[i][0][6];
             double y3 = triangles3d[i][0][7];
             double z3 = triangles3d[i][0][8];
-            double[] color = triangles3d[0][1];
+            int[] color = new int[triangles3d[0][1].length];
+            for (int j = 0 ; j < triangles3d[0][1].length ; i++ ) color[j] = (int) triangles3d[0][1][j];
+            int red = color[0];
+            int green = color[1];
+            int blue = color[2];
+
+            double ax = x2 - x1;
+            double bx = x2 - x3;
+            double ay = y2 - y1;
+            double by = y2 - y3;
+            double az = z2 - z1;
+            double bz = z2 - z3;
+            double normalX = ay*bz - az*by;
+            double normalY = az*bx - ax*bz;
+            double normalZ = ax*by - ay*bx;
+            double cos = (camX*normalX + camY*normalY + camZ*normalZ);
+            if (cos < 0){
+                normalX *= -1;
+                normalY *= -1;
+                normalZ *= -1;}
+            double normxLight = (normalX*lightX + normalY*lightY + normalZ*lightZ)/Math.sqrt((normalX*normalX + normalY*normalY + normalZ*normalZ)*(lightX*lightX + lightY*lightY + lightZ*lightZ));
+            int diffuse = (int) (1+Math.max((int)normxLight , 0));
+            color[0] = red*diffuse;
+            color[1] = green*diffuse;
+            color[2] = blue*diffuse;
 
             double[] rFirst;
             rFirst = getCoordinates(x1, y1, z1);
@@ -76,23 +115,21 @@ class Modeling extends JPanel {
             rSecond = getCoordinates(x2, y2, z2);
             double[] rThird;
             rThird = getCoordinates(x3, y3, z3);
-            double rX1 = (int) rFirst[0];
-            double rY1 = (int) rFirst[1];
-            double rX2 = (int) rSecond[0];
-            double rY2 = (int) rSecond[1];
-            double rX3 = (int) rThird[0];
-            double rY3 = (int) rThird[1];
-            double[][] result = {{rX1, rX2, rX3}, {rY1, rY2, rY3}, color};
+            int rX1 = (int) rFirst[0];
+            int rY1 = (int) rFirst[1];
+            int distance1 = (int) rFirst[2];
+            int rX2 = (int) rSecond[0];
+            int rY2 = (int) rSecond[1];
+            int distance2 = (int) rSecond[2];
+            int rX3 = (int) rThird[0];
+            int rY3 = (int) rThird[1];
+            int distance3 = (int) rThird[2];
+            int distance = distance1 + distance2 + distance3;
+            int[][] result = {{rX1, rX2, rX3}, {rY1, rY2, rY3}, color, {distance}};
             triangles2d[i] = result;
         }
     }
 
-
-//    void run() {
-//
-//
-//
-//    }
 
     public void paintComponent(Graphics g) {
 
@@ -101,15 +138,15 @@ class Modeling extends JPanel {
         int height = getHeight() / 2;
 
         for (int i = 0; i < indexOfTriangles; i++) {
-            int x1 = (int) triangles2d[i][0][0];
-            int x2 = (int) triangles2d[i][0][1];
-            int x3 = (int) triangles2d[i][0][2];
-            int y1 = (int) triangles2d[i][1][0];
-            int y2 = (int) triangles2d[i][1][1];
-            int y3 = (int) triangles2d[i][1][2];
-            int red = (int) triangles2d[i][2][0];
-            int green = (int) triangles2d[i][2][1];
-            int blue = (int) triangles2d[i][2][2];
+            int x1 =  triangles2d[i][0][0];
+            int x2 =  triangles2d[i][0][1];
+            int x3 =  triangles2d[i][0][2];
+            int y1 =  triangles2d[i][1][0];
+            int y2 =  triangles2d[i][1][1];
+            int y3 =  triangles2d[i][1][2];
+            int red =  triangles2d[i][2][0];
+            int green =  triangles2d[i][2][1];
+            int blue =  triangles2d[i][2][2];
 
 
             int[] xValues = {x1 + width, x2 + width, x3 + width};
