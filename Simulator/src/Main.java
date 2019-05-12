8f24528f1f9549dee3b7bce4b107fc9990269ae4
DOSLAN <49920936+DOSLAN@users.noreply.github.com>
2019-05-12 22:28:21 +0600

    Add files via upload

--- Simulator/src/Main.java
+++ Simulator/src/Main.java
@@ -2,51 +2,49 @@ import javax.swing.JFrame;
 public class Main
 {
     public static void main(String[] args) {
-            Modeling modeling = new Modeling();
-            modeling.inputCamera(0, 20, 500);
-            modeling.inputLight(0, 90);
+        Modeling modeling = new Modeling();
+        modeling.inputCamera(180, 20, 1000);
+        modeling.inputLight(0, 90);
 
 
+        modeling.addTriangle(0, 0, 100, 0, 71, 71, 71, 0, 71, new double[]{0, 200, 255});
+        modeling.addTriangle(0, 0, 100, -71, 0, 71, 0, 71, 71, new double[]{0, 200, 255});
+        modeling.addTriangle(0, 0, 100, 71, 0, 71, 0, -71, 71, new double[]{0, 200, 255});
+        modeling.addTriangle(0, 0, 100, 0, -71, 71, -71, 0, 71, new double[]{0, 200, 255});
 
-            modeling.addTriangle(0, 0, 100, 0, 71, 71, 71, 0, 71, new double[]{0, 200, 255});
-            modeling.addTriangle(0, 0, 100, -71, 0, 71, 0, 71, 71, new double[]{0, 200, 255});
-            modeling.addTriangle(0, 0, 100, 71, 0, 71, 0, -71, 71, new double[]{0, 200, 255});
-            modeling.addTriangle(0, 0, 100, 0, -71, 71, -71, 0, 71, new double[]{0, 200, 255});
+        modeling.addTriangle(-71, 71, 0, -71, 0, 71, 0, 71, 71, new double[]{0, 200, 255});
+        modeling.addTriangle(71, 71, 0, 0, 71, 71, 71, 0, 71, new double[]{0, 200, 255});
+        modeling.addTriangle(71, -71, 0, 71, 0, 71, 0, -71, 71, new double[]{0, 200, 255});
+        modeling.addTriangle(-71, -71, 0, 0, -71, 71, -71, 0, 71, new double[]{0, 200, 255});
 
-            modeling.addTriangle(-71, 71, 0, -71, 0, 71, 0, 71, 71, new double[]{0, 200, 255});
-            modeling.addTriangle(71, 71, 0, 0, 71, 71, 71, 0, 71, new double[]{0, 200, 255});
-            modeling.addTriangle(71, -71, 0, 71, 0, 71, 0, -71, 71, new double[]{0, 200, 255});
-            modeling.addTriangle(-71, -71, 0, 0, -71, 71, -71, 0, 71, new double[]{0, 200, 255});
+        modeling.addTriangle(-71, 0, 71, -71, 71, 0, -71, -71, 0, new double[]{0, 200, 255});
+        modeling.addTriangle(0, 71, 71, -71, 71, 0, 71, 71, 0, new double[]{0, 200, 255});
+        modeling.addTriangle(71, 0, 71, 71, 71, 0, 71, -71, 0, new double[]{0, 200, 255});
+        modeling.addTriangle(0, -71, 71, 71, -71, 0, -71, -71, 0, new double[]{0, 200, 255});
 
-            modeling.addTriangle(-71, 0, 71, -71, 71, 0, -71, -71, 0, new double[]{0, 200, 255});
-            modeling.addTriangle(0, 71, 71, -71, 71, 0, 71, 71, 0, new double[]{0, 200, 255});
-            modeling.addTriangle(71, 0, 71, 71, 71, 0, 71, -71, 0, new double[]{0, 200, 255});
-            modeling.addTriangle(0, -71, 71, 71, -71, 0, -71, -71, 0, new double[]{0, 200, 255});
+        modeling.addTriangle(0, 0, -100, -71, 0, -71, 0, 71, -71, new double[]{0, 200, 255});
+        modeling.addTriangle(0, 0, -100, 0, 71, -71, 71, 0, -71, new double[]{0, 200, 255});
+        modeling.addTriangle(0, 0, -100, 71, 0, -71, 0, -71, -71, new double[]{0, 200, 255});
+        modeling.addTriangle(0, 0, -100, 0, -71, -71, -71, 0, -71, new double[]{0, 200, 255});
 
-            modeling.addTriangle(0, 0, -100, -71, 0, -71, 0, 71, -71, new double[]{0, 200, 255});
-            modeling.addTriangle(0, 0, -100, 0, 71, -71, 71, 0, -71, new double[]{0, 200, 255});
-            modeling.addTriangle(0, 0, -100, 71, 0, -71, 0, -71, -71, new double[]{0, 200, 255});
-            modeling.addTriangle(0, 0, -100, 0, -71, -71, -71, 0, -71, new double[]{0, 200, 255});
+        modeling.addTriangle(-71, 71, -0, -71, 0, -71, 0, 71, -71, new double[]{0, 200, 255});
+        modeling.addTriangle(71, 71, -0, 0, 71, -71, 71, 0, -71, new double[]{0, 200, 255});
+        modeling.addTriangle(71, -71, -0, 71, 0, -71, 0, -71, -71, new double[]{0, 200, 255});
+        modeling.addTriangle(-71, -71, -0, 0, -71, -71, -71, 0, -71, new double[]{0, 200, 255});
 
-            modeling.addTriangle(-71, 71, -0, -71, 0, -71, 0, 71, -71, new double[]{0, 200, 255});
-            modeling.addTriangle(71, 71, -0, 0, 71, -71, 71, 0, -71, new double[]{0, 200, 255});
-            modeling.addTriangle(71, -71, -0, 71, 0, -71, 0, -71, -71, new double[]{0, 200, 255});
-            modeling.addTriangle(-71, -71, -0, 0, -71, -71, -71, 0, -71, new double[]{0, 200, 255});
+        modeling.addTriangle(-71, 0, -71, -71, 71, 0, -71, -71, 0, new double[]{0, 200, 255});
+        modeling.addTriangle(0, 71, -71, -71, 71, 0, 71, 71, 0, new double[]{0, 200, 255});
+        modeling.addTriangle(71, 0, -71, 71, 71, 0, 71, -71, 0, new double[]{0, 200, 255});
+        modeling.addTriangle(0, -71, -71, 71, -71, 0, -71, -71, 0, new double[]{0, 200, 255});
 
-            modeling.addTriangle(-71, 0, -71, -71, 71, 0, -71, -71, 0, new double[]{0, 200, 255});
-            modeling.addTriangle(0, 71, -71, -71, 71, 0, 71, 71, 0, new double[]{0, 200, 255});
-            modeling.addTriangle(71, 0, -71, 71, 71, 0, 71, -71, 0, new double[]{0, 200, 255});
-            modeling.addTriangle(0, -71, -71, 71, -71, 0, -71, -71, 0, new double[]{0, 200, 255});
-
-            modeling.toScreen();
+        modeling.toScreen();
 
 
-            JFrame frame = new JFrame("Drawing triangle");
-            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-            frame.add(modeling);
-            frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
-            frame.setVisible(true);
-
+        JFrame frame = new JFrame("Drawing triangle");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.add(modeling);
+        frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
+        frame.setVisible(true);
     }
 
 
